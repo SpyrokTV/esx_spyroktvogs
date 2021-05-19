146fe70f381012216403350dd15043e5e4200677
SpyrokTV <84471560+SpyrokTV@users.noreply.github.com>
2021-05-19 22:31:49 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,14 @@
+-- ESX_SPYROKTVOGS BY ¡SpyrokTVᵈᵉᵛ#0001
+
+Config = {}
+
+Config.playerID = true				
+Config.steamID = true				
+Config.steamURL = true				
+Config.discordID = true
+Config.xblID = true	
+Config.liveID = true
+Config.licenseID = true
+
+
+Config.Logo = "https://i.imgur.com/LfeZFvO"
