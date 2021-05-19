75bfe5720159413498cbcc7d616ff8e20bd583cf
SpyrokTV <84471560+SpyrokTV@users.noreply.github.com>
2021-05-19 22:30:09 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+author '¡SpyrokTVᵈᵉᵛ#0001'
+description 'Logs server By ¡SpyrokTVᵈᵉᵛ#0001'
+version '1.0'
+game 'gta5'
+fx_version 'bodacious'
+
+server_script {
+"server.lua",
+"config.lua"
+}
+
+client_script 'client.lua'
